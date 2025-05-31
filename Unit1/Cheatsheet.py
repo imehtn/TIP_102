2788a8f8f67dcbdac44067023736e8167c58102d
Ingrid <ingridscalant@gmail.com>
2025-05-31 14:11:46 -0400

    new changes

--- Unit1/Cheatsheet.py
+++ Unit1/Cheatsheet.py
@@ -1,3 +1,5 @@
+#print
+
 # Example 1: Printing a string
 print("Welcome to TIP102!") # Prints "Welcome to TIP102!" to the console
 
@@ -21,6 +23,8 @@ print(x + y) # Prints 8 to the console
 
 
 #-----------------------------------
+#length
+
 # Example 1: Getting the length of a list
 lst = ['a', 'b', 'c']
 lst_length = len(lst) 
@@ -33,6 +37,8 @@ print(s_length) # Output: 4
 
 
 #-----------------------------------
+#range
+
 # Example 1: Just the stop value 
 range(10) # Evaluates to the sequence: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 
@@ -44,10 +50,14 @@ range(0, 30, 5) # Evaluates to the sequence: 0, 5, 10, 15, 20, 25
 
 
 #-----------------------------------
+#sum
+
 sum([1, 2, 3, 4]) # Evaluates to 10
 
 
 #-----------------------------------
+#min
+
 # Example 1: Minimum item in a list
 min([2, 3, 4, 1, 5]) # Evaluates to 1
 
@@ -59,6 +69,8 @@ min(['a', 'b', 'c', 'aa']) # Evaluates to 'a'
 
 
 #-----------------------------------
+#max
+
 # Example 1: Maximum item in a list
 max([2, 3, 5, 1, 4]) # Evaluates to 5
 
@@ -70,6 +82,8 @@ max(['a', 'b', 'c', 'aa']) # Evaluates to 'c'
 
 
 #-----------------------------------
+#append method 
+
 # Example 1: Add an integer to the list
 lst = [1, 2, 3, 4]
 lst.append(5)
@@ -77,6 +91,8 @@ print(lst) # Prints [1, 2, 3, 4, 5]
 
 
 #-----------------------------------
+#sort method
+
 # Example 1: List of integers
 lst = [4, 2, 1, 3]
 lst.sort()
@@ -90,6 +106,8 @@ print(lst) # Prints ['a', 'b', 'c', 'd']
 
 
 #-----------------------------------
+#lower method
+
 # Example 1: Mixed case
 s = 'Hello World!'
 lowered = s.lower()
@@ -102,6 +120,8 @@ print(lowered) # Prints 'hello world'
 
 
 #-----------------------------------
+#split method
+
 # Example 1: Split along whitespace
 s = 'Never gonna give you up'
 split = s.split()
@@ -114,6 +134,8 @@ print(split) # Prints ['Never', 'gonna', 'let', 'you', 'down']
 
 
 #-----------------------------------
+#join method
+
 # Example 1: Join items in a list separated by space
 lst = ['Never', 'gonna', 'run', 'around', 'and', 'desert', 'you']
 joined = ' '.join(lst)
@@ -124,8 +146,81 @@ lst = ['Never', 'gonna', 'make', 'you', 'cry']
 joined = '-'.join(lst)
 print(joined) # Prints 'Never-gonna-make-you-cry'
 
+
 #-----------------------------------
+#strip method
+
+# Example 1: Strip whitespace
+s = '       Never gonna say goodbye       '
+stripped = s.strip()
+print(stripped) # Prints 'Never gonna say goodbye'
+
+# Example 2: Strip question marks
+s = '????Never gonna tell a lie and hurt you?????'
+stripped = s.strip('?')
+print(stripped) # Prints 'Never gonna tell a lie and hurt you'
+
+
 #-----------------------------------
+#Variables
+
+# Example 1: Integer variable
+var1 = 10
+
+# Example 2: String Variable
+var2 = "Codepath"
+
+# Example 3: Boolean Variable
+my_boolean = True
+
+print(var1) # Prints 10
+print(var2) # Prints 'Codepath'
+print(my_boolean) # Prints True
+
+# Example: Changing x from an int to a string
+x = 10
+print(x) # Prints 10
+
+x = "Hello"
+print(x) # Prints 'Hello'
+
+
 #-----------------------------------
+#Conditionals 
+
+# Example 1: Simple if statement
+x = 3
+if x > 1:
+  print("This line will execute!")
+
+if x > 5:
+  print("This line will not execute!")
+
+# Output: 'This line will execute!'
+
+# Example 2: If-Elif statement
+
+x = 10
+y = 20
+
+if x > y:
+  print("x is greater than y")
+elif y > x:
+  print("y is greater than x")
+
+# Output: 'y is greater than x'
+
+# Example 3: If-Elif-Else statement
+x = 20
+y = 20
+
+if x > y:
+  print("x is greater than y")
+elif y > x:
+  print("y is greater than x")
+else:
+  print("x and y are equal")
+
+# Output: 'x and y are equal'
 #-----------------------------------
 #-----------------------------------
