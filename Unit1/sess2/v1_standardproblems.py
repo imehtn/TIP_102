e14c754a1301314f6245e4f785cedc88d1d3b1c5
Ingrid <ingridscalant@gmail.com>
2025-06-03 08:02:51 -0400

    4 more problems sess 2 std v2

--- Unit1/sess2/v1_standardproblems.py
+++ Unit1/sess2/v1_standardproblems.py
@@ -79,24 +79,90 @@ print(sum_of_digits(num)) #4
 
 print("\nProblem 5")
 
+def final_value_after_operations(operations):
+    tigger = 1
+    
+    for operation in operations:
+        if operation == "bouncy" or operation == "flouncy":
+            tigger += 1
+        elif operation == "trouncy" or operation == "pouncy":
+            tigger -= 1
+            
+    return tigger
+
+operations = ["trouncy", "flouncy", "flouncy"]
+print(final_value_after_operations(operations)) # 2
+
+operations = ["bouncy", "bouncy", "flouncy"]
+print(final_value_after_operations(operations)) # 4
 
 #------------------------------------
 #Problem 6:
 
-print("\nproblem 6")
+print("\nProblem 6")
 
+def is_acronym(words, s):
+    acronym = ""
+    for word in words:
+        acronym += word[0].lower()
+    if acronym == s.lower():
+        return True
+    
+words = ["christopher", "robin", "milne"]
+s = "crm"
+print(is_acronym(words, s)) # True
 
 #------------------------------------
 #Problem 7:
 
 print("\nProblem 7")
 
+def make_divisible_by_3(nums):
+    count = 0
+    for num in nums:
+        if num % 3 == 0:
+            continue
+        elif (num + 1) % 3 == 0 or (num - 1) % 3 == 0:
+            count += 1
+        else:
+            count += 2
+    
+    return count
+
+nums = [1, 2, 3, 4]
+print(make_divisible_by_3(nums)) # 3
+
+nums = [3, 6, 9]
+print(make_divisible_by_3(nums)) # 0
+    
 #------------------------------------
 #Problem 8: 
 
 print("\nProblem 8")
 
+def exclusive_elemts(lst1,lst2):
+    exclusive = []
+    for elem in lst1:
+        if elem not in lst2:
+            exclusive.append(elem)
+    
+    for elem in lst2:
+        if elem not in lst1:
+            exclusive.append(elem)
+    
+    return exclusive
 
+lst1 = ["pooh", "roo", "piglet"]
+lst2 = ["piglet", "eeyore", "owl"]
+print(exclusive_elemts(lst1, lst2)) # ["pooh", "roo", "eeyore", "owl"]
+
+lst1 = ["pooh", "roo"]
+lst2 = ["piglet", "eeyore", "owl", "kanga"]
+print(exclusive_elemts(lst1, lst2)) # ["pooh", "roo", "piglet", "eeyore", "owl", "kanga"]
+
+lst1 = ["pooh", "roo", "piglet"]
+lst2 = ["pooh", "roo", "piglet"]
+print(exclusive_elemts(lst1, lst2)) # [] 
 
 #------------------------------------
 #Problem 9: 
