d56709bb95290dffc11f09d4a03b84db5337a57d
Ingrid <ingridscalant@gmail.com>
2025-06-03 08:40:48 -0400

    first 3 problems in sess 2 std v2

--- Unit1/sess2/v2_standardproblems.py
+++ Unit1/sess2/v2_standardproblems.py
@@ -4,16 +4,62 @@
 #Problem 1: 
 print("Problem 1")
 
+def are_equivalent(word1,word2):
+    first = ""
+    second = ""
+    for sub in word1:
+        first += sub
+    for sub in word2:
+        second += sub
+    return first.lower() == second.lower()
+
+word1 = ["bat", "man"]
+word2 = ["b", "atman"]
+print(are_equivalent(word1, word2)) # True
+
+word1 = ["alfred", "pennyworth"]
+word2 = ["alfredpenny", "word"]
+print(are_equivalent(word1, word2)) # False
+
+word1  = ["cat", "wom", "an"]
+word2 = ["catwoman"]
+print(are_equivalent(word1, word2)) # True
 
 #------------------------------------
 #Problem 2: 
 
 print("\nProblem 2")
 
+def count_evens(lst):
+    count = 0
+    for str in lst:
+        if len(str) % 2 == 0:
+            count += 1
+    return count
+
+lst = ["na", "nana", "nanana", "batman", "!"]
+print(count_evens(lst)) #4
+
+lst = ["the", "joker", "robin"]
+print(count_evens(lst)) #0
+
+lst = ["you", "either", "die", "a", "hero", "or", "you", "live", "long", "enough", "to", "see", "yourself", "become", "the", "villain"]
+print(count_evens(lst)) #9
+
 
 #------------------------------------
 #Problem 3: 
 print("\nProblem 3")
+
+def remove_name(people,secret_identity):
+    while secret_identity in people:
+        people.remove(secret_identity)
+    return people
+
+people = ['Batman', 'Superman', 'Bruce Wayne', 'The Riddler', 'Bruce Wayne']
+secret_identity = 'Bruce Wayne'
+print(remove_name(people, secret_identity))
+    
 
 #------------------------------------
 #Problem 4: 
