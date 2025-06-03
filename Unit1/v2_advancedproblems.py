7a679846d71b06afa62b7323858fd21cabd429ea
Ingrid <ingridscalant@gmail.com>
2025-06-02 22:25:12 -0400

    almost done with sess 1 adv v2

--- Unit1/v2_advancedproblems.py
+++ Unit1/v2_advancedproblems.py
@@ -102,24 +102,112 @@ print(minimum_boxes(meals, capacity)) #4
 print("\nProblem 5")
 
 def wealthiest_customer(accounts):
+    res = []
+    max = 0
+    for index, customer in enumerate(accounts, start=0):
+        total = sum(customer)
+        if total > max:
+            max = total
+            res = [index, total]
+            
+    return res
 
+accounts = [
+	[1, 2, 3],
+	[3, 2, 1]
+]
+print(wealthiest_customer(accounts)) #[1, 6]
+
+accounts = [
+	[1, 5],
+	[7, 3],
+	[3, 5]
+]
+print(wealthiest_customer(accounts)) #[1, 10]
+
+accounts = [
+	[2, 8, 7],
+	[7, 1, 3],
+	[1, 9, 5]
+]
+print(wealthiest_customer(accounts)) #[0, 17]
 
 #------------------------------------
 #Problem 6:
 
-print("\nproblem 6")
+print("\nProblem 6")
 
+def smaller_than_current(nums):
+    res = []
+    for num in nums:
+        count = 0
+        for j in nums:
+            if j < num:
+                count += 1
+        res.append(count)
+    
+    return res
+        
+nums = [8, 1, 2, 2, 3]
+print(smaller_than_current(nums)) # [4, 0, 1, 1, 2]
+
+nums = [6, 5, 4, 8]
+print(smaller_than_current(nums)) #[2,1,0,3]
+
+nums = [7, 7, 7, 7]
+print(smaller_than_current(nums)) #[0, 0, 0, 0]
 
 #------------------------------------
 #Problem 7:
 
 print("\nProblem 7")
 
+def diagonal_sum(grid):
+    n= len(grid)
+    total_sum = 0
+    
+    for i in range(n):
+        total_sum += grid[i][i]  # Primary diagonal
+        if i != n - 1 - i:       # Check to avoid double counting the center element
+            total_sum += grid[i][n - 1 - i]  # Secondary diagonal
+    
+    return total_sum
+        
+grid = [
+	[1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(diagonal_sum(grid)) # 25
+
+grid = [
+	[1, 1, 1, 1],
+    [1, 1, 1, 1],
+	[1, 1, 1, 1],
+    [1, 1, 1, 1]
+]
+print(diagonal_sum(grid)) # 8
+
+grid = [
+	[5]
+]
+print(diagonal_sum(grid)) # 5
+
 #------------------------------------
 #Problem 8: 
 
 print("\nProblem 8")
 
+def decrypt(code,k):
+    res = []
+    sum = 0
+    
+    if k == 0:
+        return [0] * len(code)
+    elif k < 0:
+            
+            
+    else:
 
 
     
