953295217fd7014b66b8b40483fc653de445b67d
Ingrid <ingridscalant@gmail.com>
2025-06-04 19:44:32 -0400

    sess 2 std v2 problem descriptions added

--- Unit1/sess2/v1_advancedproblems.py
+++ Unit1/sess2/v1_advancedproblems.py
@@ -4,26 +4,98 @@
 #Problem 1: 
 print("Problem 1")
 
+def transpose(matrix):
+    res = []
+    
+    #[0,0] [1,0]
+    #[0,1] [1,1] 
+    #[0,2][1,2]
+    
+    row_length = len(matrix)
+    column_length = len(matrix[0])
+    
+    for i in range(column_length):
+        row = []
+        for j in range(row_length):
+            row.append(matrix[j][i])
+        res.append(row)
+    
+    return res
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(transpose(matrix))
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+print(transpose(matrix))
 
 #------------------------------------
 #Problem 2: 
 
 print("\nProblem 2")
 
+def reverse_list(lst):
+    left = 0
+    right = len(lst)-1
+    
+    while left < right:
+        lst[left] = lst[right]
+        lst[right] = lst[left]
+        
+        left +=1
+        right -= 1
+        
+    return lst
+    
+lst = ["pooh", "christopher robin", "piglet", "roo", "eeyore"]
+print(reverse_list(lst)) #['eeyore', 'roo', 'piglet', 'roo', 'eeyore']
 
 #------------------------------------
 #Problem 3: 
 print("\nProblem 3")
+
+def remove_dupes(items):
+    for index in range(len(items)):
+        if items[index]== items[index-1]:
+            items.remove(items[index])
+    return len(items)
+
+items = ["extract of malt", "haycorns", "honey", "thistle", "thistle"]
+print(remove_dupes(items))
+
+items = ["extract of malt", "haycorns", "honey", "thistle"]
+print(remove_dupes(items))
 
 #------------------------------------
 #Problem 4: 
 
 print("\nProblem 4")
 
+def sort_by_parity(nums):
+    odds = [num for num in nums if  num % 2 ==1 ]
+    evens = [num for num in nums if  num % 2 ==0 ]
+    
+    return evens + odds
+
+nums = [3, 1, 2, 4]
+print(sort_by_parity(nums)) #[2,4,3,1]
+
+nums = [0]
+print(sort_by_parity(nums)) #[0]
+    
+
 #------------------------------------
 #Problem 5: 
 
 print("\nProblem 5")
+
+ 
 
 
 #------------------------------------
--- Unit1/sess2/v2_standardproblems.py
+++ Unit1/sess2/v2_standardproblems.py
@@ -1,7 +1,10 @@
 #UPI: Understand, Plan, and Implement.
 
 #------------------------------------
-#Problem 1: 
+#Problem 1: Given two string arrays word1 and word2, return True if the two 
+# arrays represent the same string, and False otherwise.
+
+#A string is represented by an array if the array elements concatenated in order forms the string
 print("Problem 1")
 
 def are_equivalent(word1,word2):
@@ -26,7 +29,9 @@ word2 = ["catwoman"]
 print(are_equivalent(word1, word2)) # True
 
 #------------------------------------
-#Problem 2: 
+#Problem 2: Implement a function count_evens() that accepts a list of strings 
+# lst as a parameter. The function should return the number of strings with 
+# an even length in the list.
 
 print("\nProblem 2")
 
@@ -48,7 +53,13 @@ print(count_evens(lst)) #9
 
 
 #------------------------------------
-#Problem 3: 
+#Problem 3: Write a function remove_name() to keep Batman's secret identity 
+# hidden. The function accepts a list of names people and a string 
+# secret_identity and should return the list with any instances of 
+# secret_identity removed. The list must be modified in place; you may not 
+# create any new lists as part of your solution. Relative order of the 
+# remaining elements must be maintained.
+
 print("\nProblem 3")
 
 def remove_name(people,secret_identity):
@@ -62,7 +73,8 @@ print(remove_name(people, secret_identity))
     
 
 #------------------------------------
-#Problem 4: 
+#Problem 4: Given a non-negative integer n, write a function count_digits() 
+# that returns the number of digits in n. You may not cast n to a string.
 
 print("\nProblem 4")
 
@@ -82,7 +94,9 @@ n = 0
 print(count_digits(n)) #1
 
 #------------------------------------
-#Problem 5: 
+#Problem 5: Write a function move_zeroes that accepts an integer array nums 
+# and returns a new list with all 0s moved to the end of list. The relative 
+# order of the non-zero elements in the original list should be maintained.
 
 print("\nProblem 5")
 
@@ -97,7 +111,11 @@ lst = [1, 0, 2, 0, 3, 0]
 print(move_zeroes(lst)) #[1,2,3,0,0,0]
 
 #------------------------------------
-#Problem 6:
+#Problem 6: Given a string s, reverse only all the vowels in the string and 
+# return it.
+
+#The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower
+# and upper cases and more than once.
 
 print("\nProblem 6")
 
@@ -127,7 +145,15 @@ s = "batman"
 print(reverse_vowels(s)) #batman
 
 #------------------------------------
-#Problem 7:
+#Problem 7:Batman is going on a scouting trip, surveying an area where he 
+# thinks Harley Quinn might commit her next crime spree. The area has many 
+# hills with different heights and Batman wants to find the tallest one to get
+# the best vantage point. His scout trip consists of n + 1 points at different 
+# altitudes. Batman starts his trip at point 0 with altitude 0.
+
+#Write a function highest_altitude() that accepts an integer array gain of 
+# length n where gain[i] is the net gain in altitude between points i​​​​​​ and 
+# i + 1 for all (0 <= i < n). Return the highest altitude of a point.
 
 print("\nProblem 7")
 
@@ -148,7 +174,17 @@ print(highest_altitude(gain)) #0
         
 
 #------------------------------------
-#Problem 8: 
+#Problem 8: Given a 0-indexed integer array nums, write a function 
+# left_right_difference that returns a 0-indexed integer array answer where:
+
+#len(answer) == len(nums)
+#answer[i] = left_sum[i] - right_sum[i]
+#Where:
+
+#left_sum[i] is the sum of elements to the left of the index i in the array 
+# nums. If there is no such element, left_sum[i] = 0
+#right_sum[i] is the sum of elements to the right of the index i in the array
+# nums. If there is no such element, right_sum[i] = 0
 
 print("\nProblem 8")
 
@@ -168,7 +204,8 @@ nums = [1]
 print(left_right_difference(nums)) # [0]
 
 #------------------------------------
-#Problem 9: 
+#Problem 9: Write a function common_elements() that takes in two lists lst1 
+# and lst2 and returns a list of the elements that are common to both lists.
 
 print("\nProblem 9")
 
@@ -189,7 +226,22 @@ lst2 = ["martial arts", "stealth", "master detective"]
 print(common_elements(lst1, lst2)) #[]
 
 #------------------------------------
-#Problem 10: 
+#Problem 10: Metropolis has a population n, with each citizen assigned an 
+# integer id from 1 to n. There's a rumor that Superman is an ordinary 
+# citizen among this group.
+
+#If Superman is an ordinary citizen, then:
+
+#Superman trusts nobody.
+#Everybody (except for Superman) trusts Superman.
+#There is exactly one citizen that satisfies properties 1 and 2.
+#Write a function expose_superman() that accepts a 2D array trust where
+# trust[i] = [ai, bi] representing that the person labeled ai trusts the 
+# person labeled bi. If a trust relationship does not exist in trust array, 
+# then such a trust relationship does not exist.
+
+#Return the label of Superman if he is hiding amongst the population and can 
+# be identified, or return -1 otherwise.
 
 print("\nProblem 10")
 
@@ -207,12 +259,12 @@ def expose_superman(trust,n):
     
 n = 2
 trust = [[1, 2]]
-print(expose_superman(trust, n))
+print(expose_superman(trust, n)) #2
 
 n = 3
 trust = [[1, 3], [2, 3]]
-print(expose_superman(trust, n))
+print(expose_superman(trust, n)) #3
 
 n = 3
 trust = [[1, 3], [2, 3], [3, 1]]
-print(expose_superman(trust, n))
+print(expose_superman(trust, n)) #-1
