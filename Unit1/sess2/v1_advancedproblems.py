e2d6269e9485634d1c09383ce50e0f43e89b5c53
Ingrid <ingridscalant@gmail.com>
2025-06-04 20:42:42 -0400

    added problem descriptions sess 2 adv v1

--- Unit1/sess2/v1_advancedproblems.py
+++ Unit1/sess2/v1_advancedproblems.py
@@ -1,7 +1,10 @@
 #UPI: Understand, Plan, and Implement.
 
 #------------------------------------
-#Problem 1: 
+#Problem 1: Write a function transpose() that accepts a 2D integer array 
+# matrix and returns the transpose of matrix. The transpose of a matrix is 
+# the matrix flipped over its main diagonal, swapping the rows and columns.
+
 print("Problem 1")
 
 def transpose(matrix):
@@ -36,7 +39,19 @@ matrix = [
 print(transpose(matrix))
 
 #------------------------------------
-#Problem 2: 
+#Problem 2: Write a function reverse_list() that takes in a list lst and 
+# returns elements of the list in reverse order. The list should be reversed 
+# in-place without using list slicing (e.g. lst[::-1]).
+
+#Instead, use the two-pointer approach, which is a common technique in which 
+# we initialize two variables (also called a pointer in this context) to track
+# different indices or places in a list or string, then moves the pointers to 
+# point at new indices based on certain conditions. In the most common 
+# variation of the two-pointer approach, we initialize one variable to point 
+# at the beginning of a list and a second variable/pointer to point at the 
+# end of list. We then shift the pointers to move inwards through the list 
+# towards each other, until our problem is solved or the pointers reach the 
+# opposite ends of the list.
 
 print("\nProblem 2")
 
@@ -57,7 +72,11 @@ lst = ["pooh", "christopher robin", "piglet", "roo", "eeyore"]
 print(reverse_list(lst)) #['eeyore', 'roo', 'piglet', 'roo', 'eeyore']
 
 #------------------------------------
-#Problem 3: 
+#Problem 3: Write a function remove_dupes() that accepts a sorted array items,
+# and removes the duplicates in-place such that each element appears only 
+# once. Return the length of the modified array. You may not create another 
+# array; your implementation must modify the original input array items.
+
 print("\nProblem 3")
 
 def remove_dupes(items):
@@ -73,7 +92,11 @@ items = ["extract of malt", "haycorns", "honey", "thistle"]
 print(remove_dupes(items))
 
 #------------------------------------
-#Problem 4: 
+#Problem 4: Given an integer array nums, write a function sort_by_parity() 
+# that moves all the even integers at the beginning of the array followed by 
+# all the odd integers.
+
+#Return any array that satisfies this condition.
 
 print("\nProblem 4")
 
@@ -91,7 +114,16 @@ print(sort_by_parity(nums)) #[0]
     
 
 #------------------------------------
-#Problem 5: 
+#Problem 5: Christopher Robin is helping Pooh construct the biggest hunny jar
+# possible. Help his write a function that accepts an integer array height of 
+# length n. There are n vertical lines drawn such that the two endpoints of 
+# the ith line are (i, 0) and (i, height[i]).
+
+#Find two lines that together with the x-axis form a container, such that the container contains the most honey.
+
+#Return the maximum amount of honey a container can store.
+
+#Notice that you may not slant the container.
 
 print("\nProblem 5")
 
@@ -125,7 +157,10 @@ height = [1, 1]
 print(most_honey(height))
 
 #------------------------------------
-#Problem 6:
+#Problem 6: Write a function merge_intervals() that accepts an array of 
+# intervals where intervals[i] = [starti, endi], merge all overlapping 
+# intervals, and return an array of the non-overlapping intervals that cover 
+# all the intervals in the input.
 
 print("\nProblem 6")
 
