472fb94961c6d04bdd4260fd390fd12ae9ebb342
Ingrid <ingridscalant@gmail.com>
2025-06-05 22:29:32 -0400

    sess 2 adv v2 problem 4

--- Unit1/sess2/v2_advancedproblems.py
+++ Unit1/sess2/v2_advancedproblems.py
@@ -62,6 +62,29 @@ print(squash_spaces(s)) # "With great power comes great responsibility."
 
 print("\nProblem 4")
 
+def two_sum(nums,target):
+    # Initialize two pointers
+    left = 0
+    right = len(nums) - 1
+    
+    # Iterate through the list
+    while left < right:
+        current_sum = nums[left] + nums[right]
+        if current_sum == target:
+            return [left, right]
+        elif current_sum < target:
+            left += 1
+        else:
+            right -= 1
+            
+nums = [2, 7, 11, 15]
+target = 9
+print(two_sum(nums, target))#[0,1]
+
+nums = [2, 7, 11, 15]
+target = 18
+print(two_sum(nums, target))#[1,2]
+
 #------------------------------------
 #Problem 5: 
 
