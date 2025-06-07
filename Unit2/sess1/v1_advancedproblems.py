725a4c4c3e0f182fe839ee9bee0fd5e66249cee4
Ingrid <ingridscalant@gmail.com>
2025-06-07 19:02:04 -0400

    unit 2 sess 1 std problems

--- Unit2/sess1/v1_advancedproblems.py
+++ Unit2/sess1/v1_advancedproblems.py
@@ -4,16 +4,51 @@
 #Problem 1: 
 print("Problem 1")
 
+def lineup(artists, set_times):
+    zipped = zip(artists, set_times)
+    
+    return sorted(zipped, key=lambda x: x[1])
+
+artists1 = ["Kendrick Lamar", "Chappell Roan", "Mitski", "Rosalia"]
+set_times1 = ["9:30 PM", "5:00 PM", "2:00 PM", "7:30 PM"]
+
+artists2 = []
+set_times2 = []
+
+print(lineup(artists1, set_times1)) 
+print(lineup(artists2, set_times2))
 
 #------------------------------------
 #Problem 2: 
 
 print("\nProblem 2")
 
+def get_artist_info(artist, festival_schedule):
+    return festival_schedule.get(artist, "Artist not found.")
+    
+festival_schedule = {
+    "Blood Orange": {"day": "Friday", "time": "9:00 PM", "stage": "Main Stage"},
+    "Metallica": {"day": "Saturday", "time": "8:00 PM", "stage": "Main Stage"},
+    "Kali Uchis": {"day": "Sunday", "time": "7:00 PM", "stage": "Second Stage"},
+    "Lawrence": {"day": "Friday", "time": "6:00 PM", "stage": "Main Stage"}
+}
+
+print(get_artist_info("Blood Orange", festival_schedule)) 
+print(get_artist_info("Taylor Swift", festival_schedule))
+
 
 #------------------------------------
 #Problem 3: 
 print("\nProblem 3")
+
+def total_sales(ticket_sales):
+    values = ticket_sales.values()
+    total = sum(values)
+    return total
+
+ticket_sales = {"Friday": 200, "Saturday": 1000, "Sunday": 800, "3-Day Pass": 2500}
+
+print(total_sales(ticket_sales))
 
 #------------------------------------
 #Problem 4: 
