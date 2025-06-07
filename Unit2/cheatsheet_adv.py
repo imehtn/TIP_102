ff9c528fbfefde176620e93c26be50f8a11985a0
Ingrid <ingridscalant@gmail.com>
2025-06-07 17:43:44 -0400

    chatsheet unit2

--- Unit2/cheatsheet_adv.py
+++ Unit2/cheatsheet_adv.py
@@ -53,3 +53,61 @@ print(result)
 
 #-----------------------------------
 print("\nTernary operator\n")
+
+a = 10
+b = 20
+
+# ternary operator
+max_value = a if a > b else b
+
+# normal conditional syntax
+if a > b:
+    max_value = a
+else: 
+    max_value = b
+    
+#-----------------------------------
+print("\nDisctionary Comprehensions\n")
+
+# Example 1: Map Integers to Their Square
+lst = [1, 2, 3, 4, 5, 6]
+squares = {x: x**2 for x in lst}
+print(squares) # {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36}
+
+# Example 2: Converting List of Tuples to a Dictionary
+pairs = [('a', 1), ('b', 2), ('c', 3)]
+dictionary = {key: value for key, value in pairs}
+print(dictionary)
+
+# Exmaple 3: Even Squares:
+even_squares = {x: x**2 for x in range(1, 11) if x % 2 == 0}
+print(even_squares) # {2: 4, 4: 16, 6: 36, 8: 64, 10: 100}
+
+#-----------------------------------
+print("\nTuples\n")
+
+my_tuple = ("Mario", "Luigi")
+print(my_tuple) # Prints ("Mario", "Luigi")
+
+my_tuple = ("Mario", "Luigi")
+mario = my_tuple[0] 
+luigi = my_tuple[1]
+print(mario) # Prints "Mario"
+print(luigi) # Prints "Luigi"
+
+my_tuple = (10, 20)
+#my_tuple[0] = 30 # Results in TypeError: 'tuple' object does not support item assignment
+
+#-----------------------------------
+print("\nBonus\n")
+
+# remove(x) Removes first element with value x from list.
+
+# defaultdict() Creates a dictionary with default values for new or missing 
+# keys. Helps to avoid KeyErrors!
+
+# Counter(x) Creates a dictionary that stores elements of a given iterable x 
+# as keys and their counts as values. Essentially creates a frequency 
+# dictionary for you!
+
+# d.copy() Returns a copy of the dictionary d
