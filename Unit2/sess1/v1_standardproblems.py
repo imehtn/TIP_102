22c0e8c2afa4fc0d97cdf18d70e60637febd3401
Ingrid <ingridscalant@gmail.com>
2025-06-07 21:39:50 -0400

    unit 2 sess 1 std v2

--- Unit2/sess1/v1_standardproblems.py
+++ Unit2/sess1/v1_standardproblems.py
@@ -1,7 +1,12 @@
 #UPI: Understand, Plan, and Implement.
 
 #------------------------------------
-#Problem 1: 
+#Problem 1: Given two lists of strings artists and set_times of length n, 
+# write a function lineup() that maps each artist to their set time.
+
+#An artist artists[i] has set time set_times[i]. Assume i <= 0 < n and 
+# len(artists) == len(set_times).
+
 print("Problem 1")
 
 def lineup(artists, set_times):
@@ -19,7 +24,16 @@ print(lineup(artists1, set_times1))
 print(lineup(artists2, set_times2))
 
 #------------------------------------
-#Problem 2: 
+#Problem 2: You are designing an app for your festival to help attendees have 
+# the best experience possible! As part of the application, users will be able
+# to easily search their favorite artist and find out the day, time, and stage 
+# the artist is playing at. Write a function get_artist_info() that accepts a 
+# string artist and a dictionary festival_schedule mapping artist's names to 
+# dictionaries containing the day, time, and stage they are playing on. Return
+# the dictionary containing the information about the given artist.
+
+#If the artist searched for does not exist in festival_schedule, return the 
+# dictionary {"message": "Artist not found"}.
 
 print("\nProblem 2")
 
@@ -38,7 +52,9 @@ print(get_artist_info("Taylor Swift", festival_schedule))
 
 
 #------------------------------------
-#Problem 3: 
+#Problem 3: A dictionary ticket_sales is used to map ticket type to number of 
+# tickets sold. Return the total number of tickets of all types sold.
+
 print("\nProblem 3")
 
 def total_sales(ticket_sales):
@@ -51,7 +67,13 @@ ticket_sales = {"Friday": 200, "Saturday": 1000, "Sunday": 800, "3-Day Pass": 25
 print(total_sales(ticket_sales))
 
 #------------------------------------
-#Problem 4: 
+#Problem 4: Demand for your festival has exceeded expectations, so you're 
+# expanding the festival to span two different venues. Some artists will 
+# perform both venues, while others will perform at just one. To ensure that 
+# there are no scheduling conflicts, implement a function identify_conflicts() 
+# that accepts two dictionaries venue1_schedule and venue2_schedule each 
+# mapping the artists playing at the venue to their set times. Return a 
+# dictionary containing the key-value pairs that are the same in each schedule.
 
 print("\nProblem 4")
 
@@ -78,7 +100,10 @@ venue2_schedule = {
 print(identify_conflicts(venue1_schedule, venue2_schedule))
 
 #------------------------------------
-#Problem 5: 
+#Problem 5: As part of the festival, attendees cast votes for their favorite 
+# set. Given a dictionary votes that maps attendees id numbers to the artist 
+# they voted for, return the artist that had the most number of votes. If 
+# there is a tie, return any artist with the top number of votes.
 
 print("\nProblem 5")
 
@@ -108,7 +133,14 @@ print(best_set(votes1))
 print(best_set(votes2))
 
 #------------------------------------
-#Problem 6:
+#Problem 6: You are given an array audiences consisting of positive integers 
+# representing the audience size for different performances at a music festival.
+
+#Return the combined audience size of all performances in audiences with the 
+# maximum audience size.
+
+#The audience size of a performance is the number of people who attended that
+# performance.
 
 print("\nproblem 6")
 def max_audience_performances(audiences):
@@ -128,7 +160,14 @@ print(max_audience_performances(audiences1))
 print(max_audience_performances(audiences2))
  
 #------------------------------------
-#Problem 7:
+#Problem 7: If you used a dictionary as part of your solution to 
+# max_audience_performances() in the previous problem, try reimplementing the 
+# function without using a dictionary. If you implemented 
+# max_audience_performances() without using a dictionary, try solving the 
+# problem with a dictionary.
+
+#Once you've come up with your second solution, compare the two. Is one 
+# solution better than the other? Why or why not?
 
 print("\nProblem 7")
 
@@ -157,7 +196,14 @@ print(max_audience_performances(audiences1))
 print(max_audience_performances(audiences2))
 
 #------------------------------------
-#Problem 8: 
+#Problem 8: Given an array of integers popularity_scores representing the 
+# popularity scores of songs in a music festival playlist, return the number 
+# of popular song pairs.
+
+#A pair (i, j) is called popular if the songs have the same popularity score 
+# and i < j.
+
+#Hint: number of pairs = (n x n-1)/2
 
 print("\nProblem 8")
 
@@ -181,7 +227,21 @@ print(num_popular_pairs(popularity_scores2))
 print(num_popular_pairs(popularity_scores3)) 
 
 #------------------------------------
-#Problem 9: 
+#Problem 9: You are given two strings s and t representing the stage 
+# arrangements of performers in two different performances at a music 
+# festival, such that every performer occurs at most once in s and t, and t 
+# is a permutation of s.
+
+#The stage arrangement difference between s and t is defined as the sum of 
+# the absolute difference between the index of the occurrence of each 
+# performer in s and the index of the occurrence of the same performer in t.
+
+#Return the stage arrangement difference between s and t.
+
+#A permutation is a rearrangement of a sequence. For example, [3, 1, 2] and 
+# [2, 1 , 3] are both permutations of the list [1, 2, 3].
+
+#Hint: Absolute value function
 
 print("\nProblem 9")
 
@@ -208,7 +268,16 @@ print(find_stage_arrangement_difference(s1, t1))
 print(find_stage_arrangement_difference(s2, t2))
 
 #------------------------------------
-#Problem 10: 
+#Problem 10: You're given strings vip_passes representing the types of guests 
+# that have VIP passes, and guests representing the guests you have at the 
+# music festival. Each character in guests is a type of guest you have. You 
+# want to know how many of the guests you have are also VIP pass holders.
+
+#Letters are case sensitive, so "a" is considered a different type of guest 
+# from "A".
+
+#Here is the pseudocode for the problem. Implement this in Python and explain
+# your implementation step-by-step.
 
 print("\nProblem 10")
 
@@ -232,7 +301,16 @@ print(num_VIP_guests(vip_passes1, guests1))
 print(num_VIP_guests(vip_passes2, guests2))
 
 #-------------------------------------
-#Problem 11:
+#Problem 11:Given a string pattern and a string schedule, return True if 
+# schedule follows the same pattern. Return False otherwise.
+
+#Here, "follow" means a full match, such that there is a one-to-one 
+# correspondence between a letter in pattern and a non-empty word in schedule.
+
+#You are provided with a partially implemented and buggy version of the 
+# solution. Identify and fix the bugs in the code. Then, perform a thorough 
+# code review and suggest improvements.
+
 print("\nProblem 11")
 
 def schedule_pattern(pattern, schedule):
@@ -266,7 +344,15 @@ print(schedule_pattern(pattern2, schedule2))
 print(schedule_pattern(pattern3, schedule3))
     
 #-------------------------------------
-#Problem 12: 
+#Problem 12: You are given an array of strings performer_names, and an array 
+# performance_times that consists of distinct positive integers representing 
+# the performance durations in minutes. Both arrays are of length n.
+
+#For each index i, performer_names[i] and performance_times[i] denote the name
+# and performance duration of the ith performer.
+
+#Return performer_names sorted in descending order by the performance durations.
+
 print("\nProblem 12")
     
 def sort_performers(performer_names, performance_times):
