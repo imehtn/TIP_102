30bf38cd3e156e3f77558df1101130d123252c66
Ingrid <ingridscalant@gmail.com>
2025-06-09 22:38:01 -0400

    problem 3

--- Unit2/sess1/v1_advancedproblems.py
+++ Unit2/sess1/v1_advancedproblems.py
@@ -54,6 +54,26 @@ print(can_trust_message(message2))
 #Problem 3: 
 print("\nProblem 3")
 
+def find_duplicate_chests(chests):
+    dupes = []
+    
+    from collections import Counter 
+    
+    count = Counter(chests)
+    for key, value in count.items():
+        if value == 2:
+            dupes.append(key)
+    
+    return dupes
+
+chests1 = [4, 3, 2, 7, 8, 2, 3, 1]
+chests2 = [1, 1, 2]
+chests3 = [1]
+
+print(find_duplicate_chests(chests1))
+print(find_duplicate_chests(chests2))
+print(find_duplicate_chests(chests3))
+
 #------------------------------------
 #Problem 4: 
 
