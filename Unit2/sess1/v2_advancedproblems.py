3afc0bb87e685b5d6b9d5015dee4ea417cbaf98f
Ingrid <ingridscalant@gmail.com>
2025-06-10 19:54:19 -0400

    problem 2

--- Unit2/sess1/v2_advancedproblems.py
+++ Unit2/sess1/v2_advancedproblems.py
@@ -29,7 +29,6 @@ actual_distribution = {
     "Room D": 300
 }
 
-
 print(analyze_library(library_catalog, actual_distribution))
 
 #------------------------------------
@@ -37,6 +36,13 @@ print(analyze_library(library_catalog, actual_distribution))
 
 print("\nProblem 2")
 
+def find_common_artifacts(artifacts1, artifacts2):
+    return list(set(artifacts1) & set(artifacts2))
+
+artifacts1 = ["Statue of Zeus", "Golden Vase", "Bronze Shield"]
+artifacts2 = ["Golden Vase", "Silver Sword", "Bronze Shield"]
+
+print(find_common_artifacts(artifacts1, artifacts2))
 
 #------------------------------------
 #Problem 3: 
