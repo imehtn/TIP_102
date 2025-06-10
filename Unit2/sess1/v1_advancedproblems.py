dc761c4564728c7c44e7559bd5fab2148cd456d6
Ingrid <ingridscalant@gmail.com>
2025-06-09 22:59:56 -0400

    problem 5

--- Unit2/sess1/v1_advancedproblems.py
+++ Unit2/sess1/v1_advancedproblems.py
@@ -108,6 +108,31 @@ print(is_balanced(code2))
 
 print("\nProblem 5")
 
+def find_treasure_indices(gold_amounts, target):
+    gold_map = {}
+    #indices and amts 
+    for i, amount in enumerate(gold_amounts):
+        #find complement of current amt 
+        complement = target - amount
+        #if complement is in dict, return pair 
+        if complement in gold_map:
+            return [gold_map[complement], i]
+        #add current amt to dict
+        gold_map[amount] = i
+    return []
+            
+gold_amounts1 = [2, 7, 11, 15]
+target1 = 9
+
+gold_amounts2 = [3, 2, 4]
+target2 = 6
+
+gold_amounts3 = [3, 3]
+target3 = 6
+
+print(find_treasure_indices(gold_amounts1, target1))  
+print(find_treasure_indices(gold_amounts2, target2))  
+print(find_treasure_indices(gold_amounts3, target3))  
 
 #------------------------------------
 #Problem 6:
