a6ccd313b1d93b12e345dc87ad752b191e098056
Ingrid <ingridscalant@gmail.com>
2025-06-10 18:10:35 -0400

    problem 6

--- Unit2/sess1/v1_advancedproblems.py
+++ Unit2/sess1/v1_advancedproblems.py
@@ -212,7 +212,11 @@ def organize_pirate_crew(group_sizes):
     
     return result
     
-    
+group_sizes1 = [3, 3, 3, 3, 3, 1, 3]
+group_sizes2 = [2, 1, 3, 3, 3, 2]
+
+print(organize_pirate_crew(group_sizes1))
+print(organize_pirate_crew(group_sizes2)) 
 
 #------------------------------------
 #Problem 7:Captain Blackbeard has two treasure maps represented by two 
