91e222dccdd9568869d7f0b9dd3e2b1d753dcbcb
Ingrid <ingridscalant@gmail.com>
2025-06-10 20:16:23 -0400

    problem 4

--- Unit2/sess1/v2_advancedproblems.py
+++ Unit2/sess1/v2_advancedproblems.py
@@ -77,6 +77,32 @@ print(declutter(souvenirs2, threshold))
 
 print("\nProblem 4")
 
+def num_of_time_portals(portals, destination): 
+    from collections import Counter
+    count_portals = Counter(portals)
+    count = 0
+    for portal in portals:
+        required = destination[len(portal):]
+        if required in count_portals:
+            # Decrease count if portal == required to avoid counting same index pairs
+            if portal == required:
+                count += count_portals[required] - 1
+            else:
+                count += count_portals[required]
+    
+    return count
+
+portals1 = ["777", "7", "77", "77"]
+destination1 = "7777"
+portals2 = ["123", "4", "12", "34"]
+destination2 = "1234"
+portals3 = ["1", "1", "1"]
+destination3 = "11"
+
+print(num_of_time_portals(portals1, destination1))
+print(num_of_time_portals(portals2, destination2))
+print(num_of_time_portals(portals3, destination3))
+
 #------------------------------------
 #Problem 5: 
 
